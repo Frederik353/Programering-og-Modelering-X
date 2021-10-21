b6d6be1f7b2520f84df8423315a88a365ff6bfba
Frederik353 <frederik.negaard@gmail.com>
2021-10-22 00:25:40 +0200

    small fixes

--- øving 5/main.py
+++ øving 5/main.py
@@ -1,23 +1,26 @@
 from microbit import *
 import time
-
+# import os
 
 
 while True:
     logg = True 
     display.scroll ("Press A to start logging", delay = 75)
+    time.sleep_ms(500)
     if button_a.was_pressed():
-        with open("temperature.txt", "w") as f:
+        writepath = "./temerature.txt"
+        # mode = "a" if os.path.exists(writepath) else "w"
+        mode = "w"
+        with open(writepath, mode) as f:
             while logg:
                 currentTemp = temperature()
                 f.write(str(currentTemp) + "\n")
                 print((currentTemp,))
+                time.sleep_ms(500)
                 display.clear()
-                # start = time.ticks_ms()
-                # while(time.ticks_diff(time.ticks_ms(), start) < 1000 ):
-                time.sleep_ms(1000)
+                time.sleep_ms(500)
+                display.show(Image.SQUARE_SMALL)
                 if button_a.was_pressed():
                     logg = False
-                display.show(Image.SQUARE_SMALL)
         display.show(Image.YES)
         time.sleep_ms(1000)
--- øving 5/plotter.py
+++ øving 5/plotter.py
@@ -1,5 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 data = []
 
@@ -11,15 +12,14 @@ with open("./temperature_10min.txt" , "r" ) as f:
 
 in_to_cm = 1/2.54
 
-fig_size = 14 * np.array([1, 1])
-
-
-fig_size = 14*np.array( [1, 1/1.618] )*in_to_cm 
+fig_size = 14 * np.array( [1, 1/1.618] ) * in_to_cm
 t = np.array(range(0, len(data)))/ 60
 
 fig = plt.figure(dpi = 300, figsize=fig_size)
 
 plt.grid()
+
+
 plt.plot(t,data, c="red", label="Temperatur")
 plt.xlabel("Tid [min]", fontsize = 16)
 plt.ylabel("Temperatur [\u00b0C]", fontsize = 16)
@@ -28,4 +28,5 @@ plt.legend()
 plt.tight_layout()
 plt.savefig("Temperatureplot.png", dpi = 300)
 plt.show()
+
 
